a66848002873d3cbd9e9635db60110f1fb3cdd78
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-11-20 21:33:21 +0330

    grid features update

--- components/front/features-grid.tsx
+++ components/front/features-grid.tsx
@@ -106,7 +106,7 @@ const features = [
 
 function FeaturedGrid() {
   return (
-    <section className="w-full h-full">
+    <section className="w-full h-full flex flex-col">
       <article className="mx-auto  w-full h-full flex flex-col gap-4 items-center justify-center max-w-md lg:max-w-lg text-center">
         <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
           ✨Features{' '}
@@ -120,7 +120,7 @@ function FeaturedGrid() {
           institutions.
         </h3>
       </article>
-      <article className="py-8 w-full h-[600px] grid grid-cols-5 grid-rows-5 gap-x-2 gap-y-4 items-stretch justify-items-stretch place-content-center place-items-center ">
+      <article className="py-8 w-full h-[600px] grid grid-cols-5 grid-rows-8 gap-x-2 gap-y-4 items-stretch justify-items-stretch place-content-center place-items-center ">
         <div className="relative overflow-hidden rounded-md backdrop-blur-3xl p-2 bg-foreground/10 col-span-3 row-span-3">
           <Image
             src={featuredImage}
@@ -191,6 +191,152 @@ function FeaturedGrid() {
               {features[3].title}
             </h3>
             <p className="text-center max-w-md ">{features[3].description}</p>
+          </Link>
+        </div>
+        <div className="relative overflow-hidden rounded-md backdrop-blur-3xl p-2 bg-foreground/10 col-span-3 row-span-3">
+          <Image
+            src={featuredImage}
+            alt="features"
+            fill
+            className="-z-10 opacity-15 object-cover"
+          />
+          <Link
+            href={features[4].href}
+            className="flex flex-col w-full h-full items-center justify-center gap-1.5 p-3"
+          >
+            {/* <span>{features[4].icon}</span> */}
+            <h3 className="text-center font-semibold text-base md:text-lg lg:text-xl">
+              {features[4].title}
+            </h3>
+            <p className="text-center max-w-md ">{features[4].description}</p>
+          </Link>
+        </div>
+        <div className="relative overflow-hidden rounded-md backdrop-blur-3xl p-2 bg-foreground/10 col-span-2 row-span-3">
+          <Image
+            src={featuredImage}
+            alt="features"
+            fill
+            className="-z-10 opacity-15 object-cover"
+          />
+          <Link
+            href={features[5].href}
+            className="flex flex-col w-full h-full items-center justify-center gap-1.5 p-3"
+          >
+            {/* <span>{features[1].icon}</span> */}
+            <h3 className="text-center font-semibold text-base md:text-lg lg:text-xl">
+              {features[5].title}
+            </h3>
+            <p className="text-center max-w-md ">{features[5].description}</p>
+          </Link>
+        </div>
+      </article>
+      <article className="py-8 w-full h-[600px] grid grid-cols-5 grid-rows-8 gap-x-2 gap-y-4 items-stretch justify-items-stretch place-content-center place-items-center ">
+        <div className="relative overflow-hidden rounded-md backdrop-blur-3xl p-2 bg-foreground/10 col-span-3 row-span-3">
+          <Image
+            src={featuredImage}
+            alt="features"
+            fill
+            className="-z-10 opacity-15 object-cover"
+          />
+          <Link
+            href={features[6].href}
+            className="flex flex-col w-full h-full items-center justify-center gap-1.5 p-3"
+          >
+            {/* <span>{features[6].icon}</span> */}
+            <h3 className="text-center font-semibold text-base md:text-lg lg:text-xl">
+              {features[6].title}
+            </h3>
+            <p className="text-center max-w-md ">{features[6].description}</p>
+          </Link>
+        </div>
+        <div className="relative overflow-hidden rounded-md backdrop-blur-3xl p-2 bg-foreground/10 col-span-2 row-span-3">
+          <Image
+            src={featuredImage}
+            alt="features"
+            fill
+            className="-z-10 opacity-15 object-cover"
+          />
+          <Link
+            href={features[7].href}
+            className="flex flex-col w-full h-full items-center justify-center gap-1.5 p-3"
+          >
+            {/* <span>{features[7].icon}</span> */}
+            <h3 className="text-center font-semibold text-base md:text-lg lg:text-xl">
+              {features[7].title}
+            </h3>
+            <p className="text-center max-w-md ">{features[7].description}</p>
+          </Link>
+        </div>
+        <div className="relative overflow-hidden rounded-md backdrop-blur-3xl p-2 bg-foreground/10 col-span-2 row-span-2">
+          <Image
+            src={featuredImage}
+            alt="features"
+            fill
+            className="-z-10 opacity-15 object-cover"
+          />
+          <Link
+            href={features[8].href}
+            className="flex flex-col w-full h-full items-center justify-center gap-1.5 p-3"
+          >
+            {/* <span>{features[8].icon}</span> */}
+            <h3 className="text-center font-semibold text-base md:text-lg lg:text-xl">
+              {features[8].title}
+            </h3>
+            <p className="text-center max-w-md ">{features[8].description}</p>
+          </Link>
+        </div>
+        <div className="relative overflow-hidden rounded-md backdrop-blur-3xl p-2 bg-foreground/10 col-span-3 row-span-2">
+          <Image
+            src={featuredImage}
+            alt="features"
+            fill
+            className="-z-10 opacity-15 object-cover"
+          />
+          <Link
+            href={features[9].href}
+            className="flex flex-col w-full h-full items-center justify-center gap-1.5 p-3"
+          >
+            {/* <span>{features[9].icon}</span> */}
+            <h3 className="text-center font-semibold text-base md:text-lg lg:text-xl">
+              {features[9].title}
+            </h3>
+            <p className="text-center max-w-md ">{features[9].description}</p>
+          </Link>
+        </div>
+        <div className="relative overflow-hidden rounded-md backdrop-blur-3xl p-2 bg-foreground/10 col-span-3 row-span-3">
+          <Image
+            src={featuredImage}
+            alt="features"
+            fill
+            className="-z-10 opacity-15 object-cover"
+          />
+          <Link
+            href={features[10].href}
+            className="flex flex-col w-full h-full items-center justify-center gap-1.5 p-3"
+          >
+            {/* <span>{features[10].icon}</span> */}
+            <h3 className="text-center font-semibold text-base md:text-lg lg:text-xl">
+              {features[10].title}
+            </h3>
+            <p className="text-center max-w-md ">{features[10].description}</p>
+          </Link>
+        </div>
+        <div className="relative overflow-hidden rounded-md backdrop-blur-3xl p-2 bg-foreground/10 col-span-2 row-span-3">
+          <Image
+            src={featuredImage}
+            alt="features"
+            fill
+            className="-z-10 opacity-15 object-cover"
+          />
+          <Link
+            href={features[11].href}
+            className="flex flex-col w-full h-full items-center justify-center gap-1.5 p-3"
+          >
+            {/* <span>{features[1].icon}</span> */}
+            <h3 className="text-center font-semibold text-base md:text-lg lg:text-xl">
+              {features[11].title}
+            </h3>
+            <p className="text-center max-w-md ">{features[11].description}</p>
           </Link>
         </div>
       </article>
